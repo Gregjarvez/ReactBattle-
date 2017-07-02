6528ece92e19e70ded189fb5047a5e084708aee3
GREGORY ASSASIE <gregjarvez@gmail.com>
2017-07-02 01:23:42 +0100

    nav section complete

---
+++ app/index.js
@@ -0,0 +1,1 @@
+import  React from 'react';import ReactDOM from 'react-dom';import App from "./component/App";require('./index.css');ReactDOM.render(    <App />,    document.getElementById('root')); 
